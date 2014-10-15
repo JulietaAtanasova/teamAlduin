55dd9c9454dccf0354d71ae6ceddbb345208e1b9
Velimir <velimir.gerasimov@gmail.com>
2014-10-15 17:57:10 +0300

    constants update

--- AlduinRPG/Models/BossEnemy.cs
+++ AlduinRPG/Models/BossEnemy.cs
@@ -2,8 +2,8 @@
 {
     public class BossEnemy : Enemy
     {
-        private const int DefaultMaxHealth = 120;
-        private const int DefaultAttackStrength = 50;
+        private const int DefaultMaxHealth = 500;
+        private const int DefaultAttackStrength = 20;
         private const int DefaultLevel = 1;
         public BossEnemy(Coordinates coordinates)
             : base(coordinates, DefaultMaxHealth, DefaultAttackStrength, DefaultLevel)
