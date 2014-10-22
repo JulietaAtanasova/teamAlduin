e6a9410b047902ec8553077f3bb1e42674e7205f
f1mp3r <craizup@gmail.com>
2014-10-22 13:17:25 +0300

    Fixed RenderObject
    Fixed RenderObject and removed useless Views

--- AlduinRPG/Views/RenderObject.cs
+++ AlduinRPG/Views/RenderObject.cs
@@ -3,18 +3,19 @@
     using System.Drawing;
     using System.Windows.Forms;
     using System;
+    using Models;
 
     public static class RenderObject
     {
-        public static void RenderImage(GameForm gameForm, Image image, int x, int y, int width = 0, int height = 0)
+        public static void RenderImage(GameForm gameForm, Image image, Coordinates coordinates, int width = 70, int height = 70)
         {
             try
             {
                 var picBox = new PictureBox();
-                //picBox.BackColor = Color.Transparent;
+                picBox.BackColor = Color.Transparent;
                 picBox.Image = image;
                 picBox.Parent = gameForm;
-                picBox.Location = new Point(x, y);
+                picBox.Location = new Point(coordinates.X, coordinates.Y);
                 picBox.Size = new Size(width, height);
                 gameForm.Controls.Add(picBox);
             }
